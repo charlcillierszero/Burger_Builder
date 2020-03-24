f5c8d64b72babb082b360358834eddeb3693cd21
Charl Cilliers <charl.cilliers@entelect.co.za>
2020-03-24 21:17:16 +0200

    none: added base Layout component and BurgerBuild container

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxiliry';
+
+class BurgerBuiler extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build conrtols</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuiler;
